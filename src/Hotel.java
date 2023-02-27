2963b52fa07b669b95e14494f626bb5c5d7906a2
jackson51472 <53409735+jackson51472@users.noreply.github.com>
2023-02-27 14:18:21 -0300

    Create Hotel.java

---
+++ src/Hotel.java
@@ -0,0 +1,20 @@
+public class Hotel {
+    private String nomeHotel;
+    private String cnpj;
+
+    public String getNome() {
+        return nomeHotel;
+    }
+
+    public void setNome(String nome) {
+        this.nomeHotel = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
